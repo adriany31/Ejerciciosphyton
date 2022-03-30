f3f54c4b111622564309e5460d3de8278fdde182
adriany31 <amontaa9@misena.edu.co>
2022-03-30 11:32:52 -0500

    problemas
    actualizacion de los problemas hechos en clase

---
+++ ejemplomarvel.py
@@ -0,0 +1,28 @@
+puede_volar = input("¿puede volar?")
+es_humano = input("¿es humano?")
+tiene_mascara = input("¿tiene mascara?")
+
+if puede_volar =="si":
+    if es_humano =="si":
+        if tiene_mascara =="si": 
+            print("Iron Man")
+        else: 
+            print("Capitana Marvel")
+    else:
+        if tiene_mascara =="si":
+            print("Ronan Acuser")
+        else:
+            print("Vision")
+else:
+    if es_humano =="si":
+        if tiene_mascara == "si":
+            print("Hombre araña")
+        else:
+            print("Hulk")
+    else:
+        if tiene_mascara =="si":
+            print("Black Bolt")
+        else:
+            print("Thanos") 
+            
+
