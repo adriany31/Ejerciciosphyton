b42639bc4910e80f0e52132073eb11124ff478b2
adriany31 <amontaa9@misena.edu.co>
2022-05-02 11:39:00 -0500

    actualizacion

---
+++ list2.py
@@ -0,0 +1,5 @@
+a = []
+b = int(input('Escriba un numero: '))
+for i in range(b):
+    a.append(b**3)
+print(a)
---
+++ list6.py
@@ -0,0 +1,6 @@
+a=[]
+limite=int(input('Escriba el numero limite de su lista: '))
+for i in range (limite):
+    valores=int(input('Escriba los valores: '))
+    a.append(valores**2)
+print(a)
